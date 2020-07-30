87e969eeee1b1ef17927851cc7a1a93a00253b52
alquinex <alquinex2010@hotmail.com>
2020-07-30 13:50:34 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import About from "../components/Home/About"
+import Services from "../components/Home/Services"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <Layout>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, odit?"
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+    <About />
+    <Services />
+  </Layout>
+)
