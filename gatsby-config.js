6350b94a37791e735b36b3cb756b724f0e2a69e1
Alusine Q. Sesay <alusineq.sesay@Alusines-MacBook-Pro.local>
2020-10-03 15:53:33 +0200

    comleted project

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,6 +29,14 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://newgatsby-project-backroads.netlify.app',
+        sitemap: 'https://newgatsby-project-backroads.netlify.app/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
     `gatsby-plugin-transition-link`,
